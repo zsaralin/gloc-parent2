e808132b4f272fae9efea2ecc266a9ca9aa43589
zsaralin <zsaralin@gmail.com>
2025-02-20 13:42:52 -0500

    new local branch

--- gloc-be/scores.js
+++ gloc-be/scores.js
@@ -1,5 +1,5 @@
 const {Pool} = require("pg");
-require('dotenv').config(); // Load environment variables from .env file
+require('dotenv').config({ path: __dirname + '/.env' });
 
 const pool = new Pool({
     user: process.env.DB_USER,
