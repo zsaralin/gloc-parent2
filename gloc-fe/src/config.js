1b30093f20cdf9f0ba2d1fc697fa3717b46225b8
zsaralin <saralin@antimodular.com>
2025-05-07 16:08:27 -0400

    fixing url backend

--- gloc-fe/src/config.js
+++ gloc-fe/src/config.js
@@ -1,4 +1,4 @@
-export const SERVER_URL =  "https://be.levelofconfidence.net";//"https://face-recognition-be.onrender.com"; //
+export const SERVER_URL =  "https://sandbx.levelofconfidence.net";//"https://face-recognition-be.onrender.com"; //
 
 
 let currLanguage = localStorage.getItem("language") || "en";
